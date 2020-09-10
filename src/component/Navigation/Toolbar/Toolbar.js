c8ce61c371b1897ec6040f98b1a28d8600e5029b
ankit.kumar2 <ankit.kumar2@appinventiv.com>
2020-09-11 01:39:59 +0530

    improve modal performance

--- src/component/Navigation/Toolbar/Toolbar.js
+++ src/component/Navigation/Toolbar/Toolbar.js
@@ -1,15 +1,16 @@
 import React from "react";
-import "./Toolbar.css"
+import "./Toolbar.css";
 import Logo from "../../UI/Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
-import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle"
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 const Toolbar = (props) => {
   return (
     <header className="Toolbar">
-     <DrawerToggle clicked={props.drawerToggleClicked}></DrawerToggle>
-          <Logo height="80%"/>
+      <DrawerToggle clicked={props.drawerToggleClicked}></DrawerToggle>
+      {/* <div>Menu</div> */}
+      <Logo height="80%" />
       <nav className="DesktopOnly">
-          <NavigationItems/>
+        <NavigationItems />
       </nav>
     </header>
   );
--- src/component/OrderSummary/OrderSummery.js
+++ src/component/OrderSummary/OrderSummery.js
@@ -1,31 +1,38 @@
-import React from "react";
-import "./OrderSummary.css"
+import React, { Component } from "react";
+import "./OrderSummary.css";
 import Aux from "../../hoc/Aux";
 import Button from "../UI/Button/Butoon";
-const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((key) => {
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log("order summary updating");
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((key) => {
+      return (
+        <li key={key}>
+          <span style={{ textTransform: "capitalize" }}> {key}</span> :{" "}
+          {this.props.ingredients[key]}
+        </li>
+      );
+    });
     return (
-      <li key={key}>
-        <span style={{ textTransform: "capitalize" }}> {key}</span> :{" "}
-        {props.ingredients[key]}
-      </li>
+      <Aux>
+        <h3>Order Summary</h3>
+        <p> A order summary with following ingredients :-</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.total_price}</strong>
+        </p>
+        <p>Continue to Checkout</p>
+        <Button clicked={this.props.proceed} btnType="Success">
+          Proceed
+        </Button>
+        <Button clicked={this.props.cancel} btnType="Danger">
+          Cancel
+        </Button>
+      </Aux>
     );
-  });
+  }
+}
 
-  return (
-    <Aux>
-      <h3>Order Summary</h3>
-      <p> A order summary with following ingredients :-</p>
-      <ul>
-          {ingredientSummary}
-      </ul>
-      <p>
-  <strong>Total Price: {props.total_price}</strong>
-      </p>
-      <p>Continue to Checkout</p>
-      <Button clicked={props.proceed} btnType="Success">Proceed</Button>
-      <Button clicked={props.cancel} btnType="Danger">Cancel</Button>
-    </Aux>
-  );
-};
 export default OrderSummary;
