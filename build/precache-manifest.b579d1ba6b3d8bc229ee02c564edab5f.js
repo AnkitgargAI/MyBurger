675078fcc39c08c240664593a94228ebf377aa85
ankit.kumar2 <ankit.kumar2@appinventiv.com>
2020-09-11 14:44:28 +0530

    Production build created

---
+++ build/precache-manifest.b579d1ba6b3d8bc229ee02c564edab5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb370a46266f48013721999489cc2e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7fc4aba5c70ef3a022c",
+    "url": "/static/css/main.457d74ff.chunk.css"
+  },
+  {
+    "revision": "34c732ce4d9f8f3e881a",
+    "url": "/static/js/2.16e6a298.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16e6a298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7fc4aba5c70ef3a022c",
+    "url": "/static/js/main.1e29f8d3.chunk.js"
+  },
+  {
+    "revision": "08c49aac8c00567a29d6",
+    "url": "/static/js/runtime-main.59a75140.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
